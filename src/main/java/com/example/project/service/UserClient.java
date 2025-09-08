5112bb7b88fd287d5cf0d02d11a1d734a5b75371
Robert-Mirzoyan <rmirzoyan@griddynamics.com>
2025-09-08 18:13:25 +0400

    Configure Spring Cloud Circuit Breaker for project using Resilience4J

--- src/main/java/com/example/project/service/UserClient.java
+++ src/main/java/com/example/project/service/UserClient.java
@@ -1,6 +1,8 @@
 package com.example.project.service;
 
 import com.example.project.dto.UserDto;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.security.oauth2.jwt.Jwt;
@@ -13,6 +15,8 @@ public class UserClient {
 
     private final WebClient userWebClient;
 
+    @CircuitBreaker(name = "userService", fallbackMethod = "fallbackFindUser")
+    @Retry(name = "userService")
     public UserDto findUser(String userEmail, Jwt jwt) {
         String bearer = "Bearer " + jwt.getTokenValue();
         return userWebClient.get()
@@ -21,5 +25,10 @@ public class UserClient {
                 .retrieve()
                 .bodyToMono(UserDto.class)
                 .block();
+    }
+
+    @SuppressWarnings("unused")
+    public UserDto fallbackFindUser(String userEmail, Jwt jwt, Throwable ex) {
+        throw new IllegalStateException("User service is unavailable, cannot validate " + userEmail, ex);
     }
 }
